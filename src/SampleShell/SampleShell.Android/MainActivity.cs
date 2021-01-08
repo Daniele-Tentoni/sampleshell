d7970b5c9d998ddd57b20668cd7e4117555b855c
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2021-01-08 15:07:40 +0100

    Add code to login.

--- src/SampleShell/SampleShell.Android/MainActivity.cs
+++ src/SampleShell/SampleShell.Android/MainActivity.cs
@@ -4,24 +4,27 @@
     using Android.Content.PM;
     using Android.Runtime;
     using Android.OS;
+    using Plugin.CurrentActivity;
     using Plugin.Fingerprint;
 
-    [Activity(Label = "SampleShell", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "SampleShell", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            CrossFingerprint.SetCurrentActivityResolver(() => this);
 
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
+            CrossCurrentActivity.Current.Init(this, savedInstanceState);
+            CrossFingerprint.SetCurrentActivityResolver(() => CrossCurrentActivity.Current.Activity);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
--- src/SampleShell/SampleShell/ViewModels/LoginViewModel.cs
+++ src/SampleShell/SampleShell/ViewModels/LoginViewModel.cs
@@ -1,6 +1,8 @@
 ﻿namespace SampleShell.ViewModels
 {
     using MvvmHelpers;
+    using Plugin.Fingerprint;
+    using Plugin.Fingerprint.Abstractions;
     using SampleShell.Resources.Strings;
     using System.Threading.Tasks;
     using Xamarin.Essentials;
@@ -15,11 +17,55 @@
         public Command FingerPrintLoginCommand => new Command(async () => await ExecuteFingerPrintLogin());
 
         // TODO: Here you have to add credentials 
-        private async Task ExecuteLogin() => await Shell.Current.GoToAsync("//about");
+        private async Task ExecuteLogin() => await NextPage();
 
+        // TODO: Here you control if have permissions.
         private async Task ExecuteFingerPrintLogin()
         {
+            if (IsBusy) return;
+            IsBusy = true;
 
+            try
+            {
+                /*
+                 * Before this you have to acquire the current activity in
+                 * Android CurrentActivity from James Montemagno.
+                 * See MainActivity code.
+                 */
+                var request = new AuthenticationRequestConfiguration(
+                    "Prove you have fingers!",
+                    "Because without it you can't have access");
+                var result = await CrossFingerprint.Current.AuthenticateAsync(request);
+                if (result.Authenticated)
+                {
+                    /*
+                     * Make here secret stuffs like change page.
+                     */
+                    await Device.InvokeOnMainThreadAsync(async () =>
+                    await Application.Current.MainPage.DisplayAlert(
+                        "Authentication", "You are authenticated", "Ok"));
+                    await NextPage();
+                }
+                else
+                {
+                    /*
+                     * Say to user that is not allowed to make secret stuffs.
+                     */
+                    await Device.InvokeOnMainThreadAsync(async () =>
+                    await Application.Current.MainPage.DisplayAlert(
+                        "Authentication", "You aren't authenticated", "Bad"));
+                }
+            }
+            finally
+            {
+                IsBusy = false;
+            }
         }
+
+        /// <summary>
+        /// Go to next page with Shell apis.
+        /// </summary>
+        /// <returns>Task to complete.</returns>
+        private async Task NextPage() => await Shell.Current.GoToAsync("//about");
     }
 }
