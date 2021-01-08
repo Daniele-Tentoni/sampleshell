33ef2bdb1d202690169d0854a35af3eb9239ac1f
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2021-01-08 17:13:07 +0100

    Added new strings.

--- src/SampleShell/SampleShell/Resources/Strings/About/AboutStrings.Designer.cs
+++ src/SampleShell/SampleShell/Resources/Strings/About/AboutStrings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SampleShell.Resources.Strings.Login {
+namespace SampleShell.Resources.Strings.About {
     using System;
     using System.Reflection;
     
@@ -16,21 +16,21 @@ namespace SampleShell.Resources.Strings.Login {
     [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class LoginStrings {
+    public class AboutStrings {
         
         private static System.Resources.ResourceManager resourceMan;
         
         private static System.Globalization.CultureInfo resourceCulture;
         
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal LoginStrings() {
+        internal AboutStrings() {
         }
         
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Advanced)]
         public static System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.Equals(null, resourceMan)) {
-                    System.Resources.ResourceManager temp = new System.Resources.ResourceManager("SampleShell.Resources.Strings.Login.LoginStrings", typeof(LoginStrings).Assembly);
+                    System.Resources.ResourceManager temp = new System.Resources.ResourceManager("SampleShell.Resources.Strings.About.AboutStrings", typeof(AboutStrings).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -47,18 +47,6 @@ namespace SampleShell.Resources.Strings.Login {
             }
         }
         
-        public static string FingerprintTitle {
-            get {
-                return ResourceManager.GetString("FingerprintTitle", resourceCulture);
-            }
-        }
-        
-        public static string FingerprintText {
-            get {
-                return ResourceManager.GetString("FingerprintText", resourceCulture);
-            }
-        }
-        
         public static string PageTitle {
             get {
                 return ResourceManager.GetString("PageTitle", resourceCulture);
@@ -68,30 +56,6 @@ namespace SampleShell.Resources.Strings.Login {
         public static string PageRoute {
             get {
                 return ResourceManager.GetString("PageRoute", resourceCulture);
-            }
-        }
-        
-        public static string AuthenticationTitle {
-            get {
-                return ResourceManager.GetString("AuthenticationTitle", resourceCulture);
-            }
-        }
-        
-        public static string AuthenticationSuccessText {
-            get {
-                return ResourceManager.GetString("AuthenticationSuccessText", resourceCulture);
-            }
-        }
-        
-        public static string AuthenticationFailureText {
-            get {
-                return ResourceManager.GetString("AuthenticationFailureText", resourceCulture);
-            }
-        }
-        
-        public static string AuthenticationButton {
-            get {
-                return ResourceManager.GetString("AuthenticationButton", resourceCulture);
             }
         }
     }
