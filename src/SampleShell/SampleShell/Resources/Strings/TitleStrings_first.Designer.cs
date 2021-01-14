eec85eccc71094ed0c94fd001acff27efca81cad
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2021-01-14 16:17:43 +0100

    White Labeling 3 applications.

--- src/SampleShell/SampleShell/Resources/Strings/TitleStrings_first.Designer.cs
+++ src/SampleShell/SampleShell/Resources/Strings/TitleStrings_first.Designer.cs
@@ -16,21 +16,21 @@ namespace SampleShell.Resources.Strings {
     [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class TitleStrings {
+    internal class TitleStrings_first {
         
         private static System.Resources.ResourceManager resourceMan;
         
         private static System.Globalization.CultureInfo resourceCulture;
         
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal TitleStrings() {
+        internal TitleStrings_first() {
         }
         
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Advanced)]
         internal static System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.Equals(null, resourceMan)) {
-                    System.Resources.ResourceManager temp = new System.Resources.ResourceManager("SampleShell.Resources.Strings.TitleStrings", typeof(TitleStrings).Assembly);
+                    System.Resources.ResourceManager temp = new System.Resources.ResourceManager("SampleShell.Resources.Strings.TitleStrings_first", typeof(TitleStrings_first).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
